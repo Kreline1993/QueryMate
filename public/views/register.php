4a27f5e7d43a4af3ae81f4b1492fb9a753ba6c01
Kreline1993 <n.engemann1993@gmail.com>
2025-09-22 23:07:33 +0200

    Added register.php

---
+++ public/views/register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/querymate/public/assets/css/output.css?v=1" rel="stylesheet">
+    </head>
+    <body class="min-h-screen bg-gray-500 text-gray-900">
+        <main>
+            <form action="register.php" method="post">
+                <h1>
+            </form>
+        </main>
+    </body>
+</html>
